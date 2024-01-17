84dea30c44c9c6e3bafd2d6b04e89ab3713e55a7
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-01-17 17:56:31 +0100

    feat: add `04-copy-directory` task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderName = 'files';
+const srcFolderPath = path.join(__dirname, folderName);
+const destFolderPath = path.join(__dirname, `${folderName}-copy`);
+
+fs.mkdir(destFolderPath, { recursive: true }, (err) => {
+  if (err) {
+    console.error(err);
+  }
+  copyFiles();
+});
+
+function copyFiles() {
+  fs.readdir(srcFolderPath, (err, files) => {
+    if (err) {
+      console.error(err);
+    }
+
+    files.forEach((file) => {
+      const srcFile = path.join(srcFolderPath, file);
+      const destFile = path.join(destFolderPath, file);
+      fs.copyFile(srcFile, destFile, (err) => {
+        if (err) console.error(err);
+      });
+    });
+  });
+}
